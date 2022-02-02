67d31fe7d493f15364a472c1fe262b749a707935
Tesszike99 <teszidani02@gmail.com>
2022-02-02 11:19:00 +0100

    Feature - basic design implemented

--- src/main/java/com/codecool/hackernews/News.java
+++ src/main/java/com/codecool/hackernews/News.java
@@ -1,31 +1,29 @@
 package com.codecool.hackernews;
 
-import javax.servlet.ServletException;
+import  javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "hackerNewsServlet", urlPatterns = {"/"}, loadOnStartup = 1)
-public class HackerNewServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
+@WebServlet(name = "news", urlPatterns = "/news", loadOnStartup = 2)
+public class News extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         PrintWriter out = response.getWriter();
 
         out.println(
                 "<html>\n" +
                         "<head>" +
                         "  <link rel=\"stylesheet\" type=\"text/css\" href='/static/css/site.css' />" +
-                        "    <script src='/static/js/main.js' defer></script>" +
                         "</head>\n" +
                         "<body id=body>\n" +
                         "<div class= row>" +
                         "   <div class= column>" +
-                        "       <a href= /news >Hackson news</a>" +
+                        "       <a>Hackson news</a>" +
                         "   </div>" +
                         "   <div class= column>" +
                         "       <a>Top news</a>" +
@@ -41,3 +39,4 @@ public class HackerNewServlet extends javax.servlet.http.HttpServlet {
         );
     }
 }
+
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,1 +1,16 @@
-alert("I'm from main.js");
+const api_url = "https://api.hnpwa.com/v0/news/4.json";
+async function getUrl(){
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+
+}
+
+function testMethod(){
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = "Its working now lol";
+    document.body.appendChild(newDiv);
+    console.log(document.body);
+}
+
+testMethod();
